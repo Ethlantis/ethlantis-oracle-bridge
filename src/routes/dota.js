67ea0fd332c0e91611a1c53db67bdeb40fec6531
Li Arolf Rey <liarolfrrey@gmail.com>
2018-07-19 22:19:03 +0800

    feat: Handle match result for dota 2 api

--- src/routes/dota.js
+++ src/routes/dota.js
@@ -1,14 +1,30 @@
 const Router = require('koa-router');
+const axios = require('axios');
 
 const router = new Router();
 const BASE_URL = '/api/dota';
 
 router.get(`${BASE_URL}/:id`, async (ctx) => {
   try {
+    const { params: { id } } = ctx;
+    const res = await axios(`https://api.opendota.com/api/matches/${id}`);
+
+    const { data: { radiant_win, radiant_team, dire_team } } = res;
+    let matchWinner;
+
+    if (radiant_win) {
+      matchWinner = radiant_team.name;
+    } else {
+      matchWinner = dire_team.name;
+    }
+
+    ctx.status = 200;
     ctx.body = {
-      status: 'success',
+      matchWinner,
+      id,
     };
   } catch (e) {
+    ctx.status = 400;
     console.log(e);
   }
 });
